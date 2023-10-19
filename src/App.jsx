73571b8810f7c9dcac0ac0a5ea080343aaf9cb15
Nayab-Tahir <nayabtahir321@gmail.com>
2023-10-20 01:58:51 +0500

    Converted to component - Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import './App.css'
 import Ruler from './components/Ruler/Ruler';
 import Header from './components/Header/Header';
+import Content from './components/Content/Content';
 
 export default function App() {
   return (
     <>
       <Ruler />
       <Header />
-      <div className='content-page'>
-        <div className='content-div'>
-          <div className='section1'>
-            <div className="greetings">
-              <h6>// hi, i'm nayab tahir, a...</h6>
-            </div>
-            <div className='career'>
-              <span className='career-braces'>&#123;"&nbsp;</span>
-              <span className='career-content'>_web developer|</span>
-              <span className='career-braces'>&nbsp;"&#125;</span>
-            </div>
-            <div className='arrow'>
-              <h5>==&gt;</h5>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Content />
     </>
   );
 };
---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,25 @@
+import "./Content.css";
+
+const Content = () => {
+  return (
+    <div className="content-page">
+      <div className="content-div">
+        <div className="section">
+          <div className="greetings">
+            <h6>// hi, i'm nayab tahir, a...</h6>
+          </div>
+          <div className="career">
+            <span className="career-braces">&#123;"&nbsp;</span>
+            <span className="career-content">_web developer|</span>
+            <span className="career-braces">&nbsp;"&#125;</span>
+          </div>
+          <div className="arrow">
+            <h5>==&gt;</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
