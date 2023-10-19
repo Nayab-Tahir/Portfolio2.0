cd657e7012bbaf0f1b65ccf17fa43af6d94d7108
Nayab-Tahir <nayabtahir321@gmail.com>
2023-10-20 00:14:50 +0500

    section1 added

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,19 @@ export default function App() {
       <Header />
       <div className='content-page'>
         <div className='content-div'>
-          <div className='section1'>Helooo</div>
-          <div className='section2'>Helooo2</div>
+          <div className='section1'>
+            <div className="greetings">
+              <h6>// hi, i'm nayab tahir, a...</h6>
+            </div>
+            <div className='career'>
+              <span className='career-braces'>&#123;"&nbsp;</span>
+              <span className='career-content'>_web developer|</span>
+              <span className='career-braces'>&nbsp;"&#125;</span>
+            </div>
+            <div className='arrow'>
+              <h5>==&gt;</h5>
+            </div>
+          </div>
         </div>
       </div>
     </>
