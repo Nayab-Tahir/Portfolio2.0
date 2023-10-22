d4acb6c38d884e11abe0ba1657b45b4b1a7a6d26
Nayab-Tahir <nayabtahir321@gmail.com>
2023-10-22 22:18:41 +0500

    Completed AboutSection

--- src/components/Sections/AboutSection/AboutSection.jsx
+++ src/components/Sections/AboutSection/AboutSection.jsx
@@ -16,6 +16,33 @@ const AboutSection = () => {
           </p>
         </div>
       </div>
+      <div className="about-me-desc-container">
+        <div>
+          <div className="about-me-short-desc">
+            <h5>
+              ...I have over 12 years of experience in web development, with a
+              strong background in both front-end and back-end technologies;
+            </h5>
+          </div>
+        </div>
+        <div>
+          <div className="about-me-detailed-desc">
+            <p>
+              …curabitur sit amet luctus tortor, sit amet ultrices ante.
+              Maecenas convallis dui arcu, et tempor felis consectetur vel.
+              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+              posuere cubilia curae; Sed id velit ex. Cras ultricies nisi eget
+              sem mollis, a maximus felis laoreet. Nam eget ultricies urna, vel
+              ullamcorper arcu. Quisque feugiat rhoncus arcu, a tincidunt arcu
+              lacinia ac. Maecenas molestie vel ante in finibus. Sed sit amet
+              ligula eros. In hac habitasse platea dictumst;
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="about-me-btn">
+        <a href="#">&#123;read_more&#125;</a>
+      </div>
     </Section>
   );
 };
