0414b478a56dbe6e59c692b30b7cca50465a32cf
Nayab-Tahir <nayabtahir321@gmail.com>
2023-10-25 23:39:39 +0500

    changes in terminal

--- src/components/Sections/TerminalSection/Terminal/Terminal.jsx
+++ src/components/Sections/TerminalSection/Terminal/Terminal.jsx
@@ -2,7 +2,12 @@ const Terminal = () => {
   return (
     <div className="terminal">
       <div className="terminal-head">Terminal</div>
-      <div className="terminal-body">nayabtahir ~ %&nbsp;|</div>
+      <div className="terminal-body">
+        <div className="terminal-body-title">nayabtahir ~ %&nbsp;</div>
+        <div className="terminal-body-command-container">
+          <input type="text" name="command" id="command" />
+        </div>
+      </div>
     </div>
   );
 };
