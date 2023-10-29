aa7f146de05f5a2fd1f8309a6ea7e531b94e7d03
Nayab-Tahir <nayabtahir321@gmail.com>
2023-10-29 18:45:54 +0500

    Services Module  added

---
+++ src/components/Sections/ServicesSection/ServicesSection.jsx
@@ -0,0 +1,16 @@
+import Section from "../../../UI/Section";
+import SectionHead from "../../../UI/SectionHead";
+
+const ServicesSection = () => {
+  return (
+    <Section styleClass="section-none">
+      <SectionHead
+        symbol="#"
+        title="services"
+        intro="...lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis."
+      />
+    </Section>
+  );
+};
+
+export default ServicesSection;
